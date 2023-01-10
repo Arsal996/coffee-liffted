7c828a12fdee25c3c2a4e632f71c74277745d1f9
Arslan Ahmad <115068086+Arsal996@users.noreply.github.com>
2023-01-10 14:23:33 +0500

    Update coffee.js

--- js/coffee.js
+++ js/coffee.js
@@ -1,18 +1,47 @@
 const navbar = document.querySelector('nav');
 
 window.addEventListener("scroll", function () {
-    navbar.classList.toggle("sticky", window.scrollY > 40);
+    navbar.classList.toggle("sticky", window.scrollY > 20);
 });
 
 
-const mblMenu = document.querySelector('ul');
+const mblMenu = document.querySelector('.menu-list');
 const menuBar = document.querySelector('.mbl-menu-bar');
-const closeMenu = document.querySelector('.close-menu')
 
    menuBar.addEventListener('click', function(){
          mblMenu.classList.toggle('show-mbl-menu');
-   })
-    
-   closeMenu.addEventListener('click', function(){
-        ul.style.display = 'none';
-   })
+})
+
+function searchFunction() {
+    let sf = document.getElementById("search-bar-pc");
+    if (sf.style.display === "block") {
+      sf.style.display = "none";
+    } else {
+      sf.style.display = "block";
+    }
+  }
+
+
+  function cartFunction() {
+    let sf = document.getElementById("cart-side-bar");
+    if (sf.style.display === "block") {
+      sf.style.display = "none";
+    } else {
+      sf.style.display = "block";
+    }
+  }
+
+  function closeFunction() {
+    let cart = document.getElementById("cart-side-bar");
+    let sf = document.getElementById("search-bar-pc");
+    let cm = document.querySelector(".menu-list")
+    if (cart.style.display = "block") {
+      cart.style.display = "none";
+    }
+    if (sf.style.display = "block") {
+        sf.style.display = "none";
+      }
+      if (cm.style.display = "block") {
+        cm.style.display = "none";
+      }
+  }
